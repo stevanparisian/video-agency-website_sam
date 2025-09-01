540f72e958174f1e05e00047bf652f8db71ee166
StevanDinet <stevanparisian@gmail.com>
2025-09-01 13:54:04 +0200

    feat: add SEO component and meta tags

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+import SEO from '../components/SEO';
+
+export default function Contact() {
+  return (
+    <>
+      <SEO
+        title="Contact - Video Agency"
+        description="Get in touch with our video production team"
+        image="/images/contact-og.png"
+      />
+      <h1>Contact</h1>
+    </>
+  );
+}
